14909c8a3cf913069d1e08df9a1523f756b1dc36
duy <azz01012002@gmail.com>
2026-05-21 01:09:02 +0700

    add main window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Eco Sim");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Eco Sim");
+        launch(args);
     }
 }
