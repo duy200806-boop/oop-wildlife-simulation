1d877f15c934c693b66aa70eda78665fb817f547
duy <azz01012002@gmail.com>
2026-05-21 01:30:28 +0700

    rabbit flees from wolf

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,6 +1,6 @@
 public class Rabbit extends Animal {
     public Rabbit(double x, double y) {
         super(x, y, 50);
-        strategy = new PassiveStrategy();
+        strategy = new ScaredStrategy(200);
     }
 }
