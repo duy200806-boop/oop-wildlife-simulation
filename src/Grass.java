d5d4f62a8215c0e8fc551aed96167607ce5e6870
duy <azz01012002@gmail.com>
2026-05-21 01:16:00 +0700

    add plant and grass

---
+++ src/Grass.java
@@ -0,0 +1,5 @@
+public class Grass extends Plant {
+    public Grass(double x, double y) {
+        super(x, y);
+    }
+}
---
+++ src/Plant.java
@@ -0,0 +1,9 @@
+public abstract class Plant extends Entity {
+    public Plant(double x, double y) {
+        super(x, y);
+    }
+
+    @Override
+    public void update(double dt) {
+    }
+}
