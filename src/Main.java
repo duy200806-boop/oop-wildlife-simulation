e2bb98499f43f024449850a66af49af1d20ca73a
duy <azz01012002@gmail.com>
2026-05-21 01:21:10 +0700

    draw rabbit on screen

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,14 @@ import javafx.application.Application;
 import javafx.animation.AnimationTimer;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class Main extends Application {
     private World world;
+    private Canvas canvas;
 
     @Override
     public void start(Stage stage) {
@@ -13,7 +17,8 @@ public class Main extends Application {
         world.add(new Rabbit(400, 300));
         world.add(new Rabbit(200, 200));
 
-        StackPane root = new StackPane();
+        canvas = new Canvas(800, 600);
+        Pane root = new Pane(canvas);
         Scene scene = new Scene(root, 800, 600);
 
         stage.setTitle("Eco Sim");
@@ -32,9 +37,23 @@ public class Main extends Application {
                 double dt = (now - last) / 1e9;
                 last = now;
                 world.tick(dt);
+                render();
             }
         };
         timer.start();
+    }
+
+    private void render() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.LIGHTGREEN);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        for (Entity e : world.getEntities()) {
+            if (e instanceof Rabbit) {
+                gc.setFill(Color.WHITE);
+                gc.fillOval(e.getX() - 5, e.getY() - 5, 10, 10);
+            }
+        }
     }
 
     public static void main(String[] args) {
